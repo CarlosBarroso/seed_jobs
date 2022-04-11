15c1157aa568dae76fa39816c0b419cf00943516
CarlosBarroso <cbc_sg@yahoo.es>
2022-04-11 11:24:50 +0200

    Update seed_1.groovy

--- groovy_files/seed_1.groovy
+++ groovy_files/seed_1.groovy
@@ -1,9 +1,4 @@
 job("seed_1") {
-    description "Builds MyProject from master branch." 
-    logRotator(-1, 15, 1, -1)
-    parameters {
-        stringParam('COMMIT', 'HEAD', 'Commit to build')
-    }
     definition {
         cpsScm {
             scm {

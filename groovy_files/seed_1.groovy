7fbed7dc874e7c7c36d1d4ca118800f522440958
CarlosBarroso <cbc_sg@yahoo.es>
2022-04-11 10:47:35 +0200

    Update seed_1.groovy

--- groovy_files/seed_1.groovy
+++ groovy_files/seed_1.groovy
@@ -3,18 +3,7 @@ job("seed_1") {
     parameters {
         stringParam('COMMIT', 'HEAD', 'Commit to build')
     }
-    scm {
-        git {
-            remote {
-                url  "https://github.com/CarlosBarroso/seed_jobs.git",
-                branch "origin/master"
-            }
-            extensions {
-                wipeOutWorkspace()
-                localBranch master
-            }
-        }
-    }
+
     steps {
         shell "echo seed job"
     }

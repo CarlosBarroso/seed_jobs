0e2b61ced06cf9cbcd2008845731f6d2cc2b3e48
CarlosBarroso <cbc_sg@yahoo.es>
2022-04-11 11:39:17 +0200

    Update seed_1.groovy

--- groovy_files/seed_1.groovy
+++ groovy_files/seed_1.groovy
@@ -3,11 +3,10 @@ pipelineJob("seed_3") {
         cpsScm {
             remote {
                 name('origin')
-                    url("https://github.com/CarlosBarroso/seed_jobs.git")
-                 }
-                 branch('master')
-            }
-            scriptPath("jenkinsfiles/seed_1.jenkinsfile")
+                url("https://github.com/CarlosBarroso/seed_jobs.git")
+              }
+              branch('master')
         }
+        scriptPath("jenkinsfiles/seed_1.jenkinsfile")
     }
 }
